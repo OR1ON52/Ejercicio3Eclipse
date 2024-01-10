97df0880488848ccbb9cbaea2147b5686a4d4eba
sofia <adriansutil2004@gmail.com>
2024-01-10 13:32:28 +0100

    isnt it lovely all alone

--- EjercicioGit3/src/ejercicioGit3/EjercicioGit3.java
+++ EjercicioGit3/src/ejercicioGit3/EjercicioGit3.java
@@ -1,8 +1,24 @@
 package ejercicioGit3;
-public class EjercicioGit3 {
 
-	public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class EjercicioGit3 {
+    public static int contador(String texto) {
+    	if(texto == null || texto.isEmpty()) {
+    		return 0;
+    	}
+    	return texto.length();
+    }
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Introduce una frase");
+		String frase = lector.readLine();
+		int numPalabras = contador(frase);
+		System.out.println(numPalabras);
+		
 
 	}
 
